87a6bfbb610fac91ecab321ef8fd06b774223e6b
Yihuan Wang <yihuan.w10@gmail.com>
2025-12-17 14:00:48 -0500

    Set up PyInstaller

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
 import sys
+import os
 from PyQt5.QtWidgets import QApplication
+from PyQt5.QtGui import QFontDatabase
 from ui import MainWindow
 
 app = QApplication(sys.argv)
 
 window = MainWindow()
 
+# pyinstaller
+if getattr(sys, "frozen", False):
+    base_path = sys._MEIPASS
+else:
+    base_path = os.path.dirname(__file__)
+
 # apply QSS
-with open("style.qss", "r") as f:
+qss_path = os.path.join(base_path, "style.qss")
+with open(qss_path, "r", encoding="utf-8") as f:
     app.setStyleSheet(f.read())
+
+# apply font
+font_path = os.path.join(base_path, "assets/Inter/Inter-VariableFont_opsz,wght.ttf")
+QFontDatabase.addApplicationFont(font_path)
 
 window.showMaximized()
 
